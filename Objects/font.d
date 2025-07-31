6f1ac22ac041ac10830ef09da9bfb7080afa20e5
nathansho <3339859631@qq.com>
2025-07-31 21:39:18 +0800

    移除了gitignore文件的一些跟踪

--- Objects/font.d
+++
@@ -1,4 +0,0 @@
-.\objects\font.o: Hardware\OLED\font.c
-.\objects\font.o: Hardware\OLED\font.h
-.\objects\font.o: D:\Ceil5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\font.o: D:\Ceil5\ARM\ARMCC\Bin\..\include\string.h
--- Objects/kalman.d
+++
@@ -1,6 +0,0 @@
-.\objects\kalman.o: User\IMU\Kalman.c
-.\objects\kalman.o: User\IMU\kalman.h
-.\objects\kalman.o: D:\Ceil5\ARM\ARMCC\Bin\..\include\math.h
-.\objects\kalman.o: D:\Ceil5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\kalman.o: .\Core\../DEFINE/define_file.h
-.\objects\kalman.o: D:\Ceil5\ARM\ARMCC\Bin\..\include\stdint.h
